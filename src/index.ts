970edf77ed22b52d297f3257469db731ed89cb8f
RoXoM <xyy94813@sina.com>
2020-06-10 23:03:45 +0800

    fix: semantic error TS2343

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 /* eslint import/no-unresolved:"off" */
 /* eslint import/extensions:"off" */
-export { default } from './sentryMiddleware';
+import sentryMiddleware from './sentryMiddleware';
+
 export * from './sentryMiddleware';
+
+export default sentryMiddleware;
