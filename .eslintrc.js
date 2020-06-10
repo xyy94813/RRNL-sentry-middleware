8c4c3eedfb7ad19f63738988a93b08aa4b774022
RoXoM <xyy94813@sina.com>
2020-06-10 22:30:55 +0800

    chore: update eslint conf

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,5 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: './tsconfig.json',
-  },
   plugins: ['@typescript-eslint'],
   extends: ['airbnb-base'],
   rules: {
